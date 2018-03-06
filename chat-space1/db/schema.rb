f94245fd23f77c187963a09f4d4c16edf05aafbc
github user name <adetokunbopaker@yahoo.co.jp>
2018-03-06 18:15:07 +0900

    Make  user management function

--- chat-space1/db/schema.rb
+++ chat-space1/db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20180303152556) do
     t.text     "text",       limit: 65535
     t.integer  "user_id"
     t.integer  "group_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20180303152556) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "nickname"
+    t.string   "name"
     t.integer  "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
